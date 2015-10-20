9dcc8bbabd1b37e7409843e68b3555edbe53a862
craigmiller160 <craigmiller160@gmail.com>
2015-10-19 21:29:29 -0400

    Revert "Add New Constructor to ScJoinHolder"
    This reverts commit b0e2a1d0391f45fe449043416eb42071639d601b.

--- src/main/java/io/craigmiller160/school/entity/ScJoinHolder.java
+++ src/main/java/io/craigmiller160/school/entity/ScJoinHolder.java
@@ -42,10 +42,6 @@ implements JoinHolder, Comparable<ScJoinHolder>, Serializable{
 		this.scId = Integer.parseInt(sId + cId);
 	}
 	
-	public ScJoinHolder(Course course, Student student){
-		this(student, course);
-	}
-	
 	public int getScId() {
 		return scId;
 	}
