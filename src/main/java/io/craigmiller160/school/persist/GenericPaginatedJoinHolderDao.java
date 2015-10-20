9f9a0c1f018cb02e52df8b89cdc979f8a5ada48a
craigmiller160 <craigmiller160@gmail.com>
2015-10-19 23:54:39 -0400

    Separated JoinHolder DAO Pagination Methods Into New Interface
    + Created GenericPaginatedJoinHolderDao, extending GenericJoinHolderDao.
    + Moved previous and next pagination methods from GenericJoinHolderDao
    into new interface.

---
+++ src/main/java/io/craigmiller160/school/persist/GenericPaginatedJoinHolderDao.java
@@ -0,0 +1,14 @@
+package io.craigmiller160.school.persist;
+
+import java.util.List;
+
+public interface GenericPaginatedJoinHolderDao<T> 
+extends GenericJoinHolderDao<T>, GenericPaginatedDao<T>{
+
+	<U> List<T> getPreviousJoinsFor(Class<U> joinedEntityType, 
+			int entityId, int firstId, int numRecords);	
+	
+	<U> List<T> getNextJoinsFor(Class<U> joinedEntityType, 
+			int entityId, int lastId, int numRecords);
+	
+}
