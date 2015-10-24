ba32900df154e57666d7415f2ff17e3be5b686fa
craigmiller160 <craigmiller160@gmail.com>
2015-10-24 16:05:11 -0400

    Revert "BS Commit, goign to revert"
    This reverts commit 64c1837a223523594fc6e03b4f31a059c80fe0c7.

--- src/main/java/io/craigmiller160/school/controller/AdminCourseController.java
+++ src/main/java/io/craigmiller160/school/controller/AdminCourseController.java
@@ -1,33 +1,16 @@
 package io.craigmiller160.school.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import io.craigmiller160.school.entity.Course;
-import io.craigmiller160.school.persist.GenericPaginatedJoinHolderService;
 
 @Controller
 @RequestMapping (value="/admin/course")
 public class AdminCourseController {
 
-	private final GenericPaginatedJoinHolderService schoolService;
-	
-	@Autowired
-	public AdminCourseController(GenericPaginatedJoinHolderService schoolService){
-		this.schoolService = schoolService;
-	}
-	
 	@RequestMapping(value="/all", method=RequestMethod.GET)
-	public String getAllCourses(Model model){
-		List<Course> courses = schoolService.getNextEntities(
-				Course.class, 0, 10);
-		model.addAttribute("courses", courses);
-		
+	public String getAllCourses(){
+		System.out.println("Get all courses");
 		return "courses";
 	}
 	
--- src/main/webapp/WEB-INF/jsp/courses.jsp
+++ src/main/webapp/WEB-INF/jsp/courses.jsp
@@ -3,28 +3,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<%@ include file="stub/css-stub.jsp" %>
-<%@ include file="stub/jstl-stub.jsp" %>
-<%@ include file="stub/spring-stub.jsp" %>
-<link href="resources/css/classes-ids-styles.css" rel="stylesheet" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title><spring:message code="admin.course.page.title"/></title>
+<title>Insert title here</title>
 </head>
 <body>
 
-	<div class="wrapper">
-	
-		<header class="header-admin">
-			<div class="logo">
-				<a href="welcome"><img src="resources/img/ehslogo.png" height="70px"/></a>
-			</div>
-			
-			<div class="header-title">
-				<h1><spring:message code="admin.main.header.title"/></h1>
-			</div>
-		</header>
-	
-	</div>
+	<h1>Courses Page</h1>
 
 </body>
 </html>
