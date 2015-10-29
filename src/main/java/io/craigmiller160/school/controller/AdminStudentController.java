c5b743d32b03dc54adf3dce9672c0d4f7f01efbc
craigmiller160 <craigmiller160@gmail.com>
2015-10-29 19:26:34 -0400

    Added Students List Retrieval to Students Controller
    + Added call to SchoolDataService class to retrieve a list of students
    to AdminStudentController. This is done in a paginated fashion.

--- src/main/java/io/craigmiller160/school/controller/AdminStudentController.java
+++ src/main/java/io/craigmiller160/school/controller/AdminStudentController.java
@@ -1,27 +1,35 @@
 package io.craigmiller160.school.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import io.craigmiller160.school.entity.Student;
 import io.craigmiller160.school.service.GenericEntityServiceBean;
 
 @Controller
 @RequestMapping (value="/admin/student")
 public class AdminStudentController {
 
-	private final GenericEntityServiceBean schoolService;
+	private final GenericEntityServiceBean service;
 	
 	@Autowired (required=true)
-	public AdminStudentController(GenericEntityServiceBean schoolService){
-		this.schoolService = schoolService;
+	public AdminStudentController(GenericEntityServiceBean service){
+		this.service = service;
 	}
 	
 	@RequestMapping (value="/all", method=RequestMethod.GET)
-	public String getAllStudents(Model model){
-		
+	public String getAllStudents(Model model, @RequestParam int page){
+		//TODO allow for variable page sizes chosen by the user.
+		List<Student> students = service.getEntitiesByPage(
+				Student.class, page, 10);
+		model.addAttribute("students", students);
+		model.addAttribute("page", page);
 		
 		return "students";
 	}
