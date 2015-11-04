1190bd04c364476a0dd5ce025b8ef0108cb3416e
craigmiller160 <craigmiller160@gmail.com>
2015-11-04 16:58:52 -0500

    Created Email Entity
    + Created email entity for defining email addresses for student and
    admin entities.
    + Many To One Mapping to Owner entity not complete because Owner class
    not finished yet.

---
+++ src/main/java/io/craigmiller160/school/entity/Email.java
@@ -0,0 +1,60 @@
+package io.craigmiller160.school.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity (name="email")
+public class Email {
+
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO)
+	private int emailId;
+	
+	@Enumerated (EnumType.STRING)
+	@Column (name="email_type")
+	private EmailType emailType;
+	
+	@Column (name="email_address", length=255)
+	private String emailAddress;
+	
+	//TODO many to one mapping goes here
+	private Owner owner;
+
+	public int getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(int emailId) {
+		this.emailId = emailId;
+	}
+
+	public EmailType getEmailType() {
+		return emailType;
+	}
+
+	public void setEmailType(EmailType emailType) {
+		this.emailType = emailType;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+	
+}
