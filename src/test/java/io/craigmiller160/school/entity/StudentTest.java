f890e295e37213a2789f9c1cff7d5197a72f83d4
craigmiller160 <craigmiller160@gmail.com>
2015-11-10 14:16:37 -0500

    Updated StudentTest For ID Type Long
    + Updated StudentTest test class for Student entity ID type
    being changed to Long.

--- src/test/java/io/craigmiller160/school/entity/StudentTest.java
+++ src/test/java/io/craigmiller160/school/entity/StudentTest.java
@@ -64,7 +64,7 @@ public class StudentTest{
 		//Create test data
 		Student student = new Student();
 		assertNotNull(student);
-		student.setStudentId(1);
+		student.setStudentId(new Long(1));
 		student.setFirstName("Joe");
 		student.setLastName("Dirt");
 		student.setBirthDate(LocalDate.of(1988, 10, 26));
@@ -72,7 +72,7 @@ public class StudentTest{
 		student.setGrade(5);
 		
 		//Test the values
-		assertEquals("ID is wrong", student.getStudentId(), 1);
+		assertEquals("ID is wrong", student.getStudentId(), new Long(1));
 		assertEquals("First Name is wrong", student.getFirstName(), "Joe");
 		assertEquals("Last Name is wrong", student.getLastName(), "Dirt");
 		assertEquals("Birth Date is wrong", student.getBirthDate(), 
@@ -93,15 +93,15 @@ public class StudentTest{
 		
 		//The base object to be compared
 		Student student1 = new Student();
-		student1.setStudentId(1);
+		student1.setStudentId(new Long(1));
 		
 		//Same type, doesn't match
 		Student student2 = new Student();
-		student2.setStudentId(2);
+		student2.setStudentId(new Long(2));
 		
 		//Same type, should match
 		Student student3 = new Student();
-		student3.setStudentId(1);
+		student3.setStudentId(new Long(1));
 		
 		//Test for accurate comparison
 		assertFalse("ID mismatch", student1.equals(student2));
@@ -117,15 +117,15 @@ public class StudentTest{
 	public void testCompareTo(){
 		//The base object to be compared
 		Student student1 = new Student();
-		student1.setStudentId(1);
+		student1.setStudentId(new Long(1));
 		
 		//Same type, doesn't match
 		Student student2 = new Student();
-		student2.setStudentId(2);
+		student2.setStudentId(new Long(2));
 		
 		//Same type, should match
 		Student student3 = new Student();
-		student3.setStudentId(1);
+		student3.setStudentId(new Long(1));
 		
 		//Test for accurate comparison
 		assertEquals("Less than test", student1.compareTo(student2), -1);
