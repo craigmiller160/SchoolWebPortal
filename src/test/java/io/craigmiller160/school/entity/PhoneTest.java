ebb759f4f8c539739982b7e471f6493a58c9c208
craigmiller160 <craigmiller160@gmail.com>
2015-11-10 12:11:43 -0500

    Updated PhoneTest For New Equals Comparisons
    + Updated PhoneTest test class for new equals method comparisons.

--- src/test/java/io/craigmiller160/school/entity/PhoneTest.java
+++ src/test/java/io/craigmiller160/school/entity/PhoneTest.java
@@ -19,7 +19,16 @@ public class PhoneTest {
 	public void testConstructorArgs(){
 		//Create test data
 		Phone phone = new Phone(PhoneType.HOME,
-				"732", "613", "1234", "5678"){};
+				"732", "613", "1234", "5678"){
+			//Impliment these methods with dummy values
+			//They aren't needed for this test anyway.
+			public int hashCode(){
+				return 1;
+			}
+			public boolean equals(Object obj){
+				return false;
+			}
+		};
 		
 		//Test the values
 		assertNotNull("Is null", phone);
@@ -35,7 +44,16 @@ public class PhoneTest {
 	@Test
 	public void testFields(){
 		//Create test data
-		Phone phone = new Phone(){};
+		Phone phone = new Phone(){
+			//Impliment these methods with dummy values
+			//They aren't needed for this test anyway.
+			public int hashCode(){
+				return 1;
+			}
+			public boolean equals(Object obj){
+				return false;
+			}
+		};
 		phone.setPhoneId(1);
 		phone.setPhoneType(PhoneType.HOME);
 		phone.setAreaCode("732");
@@ -56,42 +74,44 @@ public class PhoneTest {
 	//Anonymous instantiation of abstract class, needs @SuppressWarnings
 	@SuppressWarnings("serial")
 	@Test
-	public void testEquals(){
-		//An invalid type object
-		Object o = new Object();
-		
-		//The base object to be compared
-		Phone phone1 = new Phone(){};
-		phone1.setPhoneId(1);
-		
-		//Same type, doesn't match
-		Phone phone2 = new Phone(){};
-		phone2.setPhoneId(2);
-		
-		//Same type, should match
-		Phone phone3 = new Phone(){};
-		phone3.setPhoneId(1);
-		
-		//Test for accurate comparison
-		assertFalse("ID mismatch", phone1.equals(phone2));
-		assertFalse("Type mismatch", phone1.equals(o));
-		assertTrue("Perfect match", phone1.equals(phone3));
-	}
-	
-	//Anonymous instantiation of abstract class, needs @SuppressWarnings
-	@SuppressWarnings("serial")
-	@Test
 	public void testCompareTo(){
 		//The base object to be compared
-		Phone phone1 = new Phone(){};
+		Phone phone1 = new Phone(){
+			//Impliment these methods with dummy values
+			//They aren't needed for this test anyway.
+			public int hashCode(){
+				return 1;
+			}
+			public boolean equals(Object obj){
+				return false;
+			}
+		};
 		phone1.setPhoneId(1);
 		
 		//Same type, doesn't match
-		Phone phone2 = new Phone(){};
+		Phone phone2 = new Phone(){
+			//Impliment these methods with dummy values
+			//They aren't needed for this test anyway.
+			public int hashCode(){
+				return 1;
+			}
+			public boolean equals(Object obj){
+				return false;
+			}
+		};
 		phone2.setPhoneId(2);
 		
 		//Same type, should match
-		Phone phone3 = new Phone(){};
+		Phone phone3 = new Phone(){
+			//Impliment these methods with dummy values
+			//They aren't needed for this test anyway.
+			public int hashCode(){
+				return 1;
+			}
+			public boolean equals(Object obj){
+				return false;
+			}
+		};
 		phone3.setPhoneId(1);
 		
 		//Test for accurate comparison
@@ -135,6 +155,54 @@ public class PhoneTest {
 		//Test if it worked
 		assertNotNull("Post-Test should not be null", 
 				phone.getAdministrator());
+	}
+	
+	@Test
+	public void testEquals(){
+		//An invalid type object
+		Object o = new Object();
+		
+		//The base object to be compared
+		PhoneAdmin phone1 = new PhoneAdmin();
+		setPhone1(phone1);
+		phone1.setPhoneId(1);
+		
+		//Same type, won't match
+		PhoneAdmin phone2 = new PhoneAdmin();
+		setPhone2(phone2);
+		phone2.setPhoneId(2);
+		
+		//Same type, should match
+		PhoneAdmin phone3 = new PhoneAdmin();
+		setPhone1(phone3);
+		phone3.setPhoneId(1);
+		
+		//Different phone type, same content, won't match
+		PhoneStudent phone4 = new PhoneStudent();
+		setPhone1(phone4);
+		phone4.setPhoneId(1);
+		
+		//Test for accurate comparison
+		assertFalse("ID mismatch", phone1.equals(phone2));
+		assertFalse("Type mismatch", phone1.equals(o));
+		assertFalse("Phone Class mismatch", phone1.equals(phone4));
+		assertTrue("Perfect match", phone1.equals(phone3));
+	}
+	
+	private void setPhone1(Phone phone){
+		phone.setPhoneType(PhoneType.HOME);
+		phone.setAreaCode("732");
+		phone.setPrefix("555");
+		phone.setSuffix("6789");
+		phone.setExtension("x1234");
+	}
+	
+	private void setPhone2(Phone phone){
+		phone.setPhoneType(PhoneType.WORK);
+		phone.setAreaCode("908");
+		phone.setPrefix("666");
+		phone.setSuffix("1234");
+		phone.setExtension(null);
 	}
 	
 }
